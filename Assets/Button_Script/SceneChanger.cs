d5a4483ad908c63eaf1439c6a5c3bf627d40a904
Hiragi0000 <vantan.tori@gmail.com>
2026-06-24 16:41:44 +0900

    program

--- Assets/Button_Script/SceneChanger.cs
+++ Assets/Button_Script/SceneChanger.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
-    public GameObject a;
-    public GameObject b;
-    public GameObject c;
-    public GameObject gameObject;
+    public Button a;
+    public Button B;
+    public Button c;
+
     [SerializeField] private string nextSceneName;
     public void ChangeScene()
     {
         SceneManager.LoadScene(nextSceneName); // シーン名で指定
     }
+    void Start()
+    {
+        if (a != null)
+        {
+            a.onClick.AddListener(ChangeScene);
+
+        }
+    }
+
 }
