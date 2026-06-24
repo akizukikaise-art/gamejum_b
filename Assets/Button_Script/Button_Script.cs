33388bfe28d4eae468cf5d362c5fbaece9488614
aki-art <akizuki.kaise@gmail.com>
2026-06-24 12:26:48 +0900

    DataKeep

---
+++ Assets/Button_Script/Button_Script.cs
@@ -0,0 +1,26 @@
+//using UnityEngine;
+
+//public class Button_Script : MonoBehaviour
+//{
+//    public GameObject Button1;
+//    public GameObject Button2;
+//    public GameObject Button3;
+
+//    [SerializeField] private int B1_nonderiti;
+//    [SerializeField] private int B2_nonderiti;
+//    [SerializeField] private int B3_nonderiti;
+
+//    private int TotalNonderi;
+
+//    public void OnClick()
+//    {
+//        if (Button1 == true) TotalNonderi += B1_nonderiti;
+//        //Button1 = B1_nonderiti;
+//        //Button2 = B2_nonderiti;
+//        //Button3 = B3_nonderiti;
+
+//        //TotalNonderi += (B1_nonderiti+ B2_nonderiti+ B3_nonderiti);
+
+//        Debug.Log(TotalNonderi);
+//    }
+//}
